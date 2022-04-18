c73088b356e4ffd67e39bcdb032936f8c2cf03b7
BartlomiejPrusicki <91379817+BartlomiejPrusicki@users.noreply.github.com>
2022-04-18 08:31:32 +0200

    Add project files.

---
+++ DiagonalLength/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Calculation of the diagonal length of a rectangle");
+
+double a, b, diagonal;
+
+Console.WriteLine("Enter page a");
+a = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter page b");
+b = double.Parse(Console.ReadLine());
+
+diagonal = (System.Math.Pow(a, 2) + System.Math.Pow(b, 2));
+Console.WriteLine("Diagonal has " + System.Math.Sqrt(diagonal));
+
+Console.Read();
